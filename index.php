71f46887b3658084d600aac2e7ff8fc1f5d1560b
Dipeshrestha_7 <dipstha061@gmail.com>
2024-05-26 07:56:38 +0545

    having some change in status in login and signin

--- index.php
+++ index.php
@@ -4,18 +4,26 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Tipanni | Home</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="index.css">
 </head>
 <body>
   
   <header>
     <nav>
-      <div class="navleft">
-        <h1><a href="index.php">Tippani</a></h1>
-      </div>
-      <div class="navright">
-        <a href="index.login.php">Login</a>
-      </div>
+          <div class="navleft">
+              <h1><a href="index.php">Tippani</a></h1>
+          </div>
+
+        <div class="navright">
+              <div class="login">
+                  <a href="index.login.php">Log In</a>
+              </div>
+
+              <div class="signin">
+                  <a href="index.signin.php">Sign In</a>
+              </div>
+        </div>
+      
     </nav>
   </header>
   <div class="content-container">
